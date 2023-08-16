ffefe8518c6fa4ab871564796fc2caac98b8f9cd
Turbo <dev@turbo.ooo>
2023-08-16 08:41:43 -0500

    feat: run MTR and Dig concurrently

--- latency_monitor/dig.py
+++ latency_monitor/dig.py
@@ -1,4 +1,5 @@
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 from os import environ
 
 from influxdb_client import Point
@@ -7,26 +8,33 @@ from .latency_monitor import app
 from .utils import parse_dig
 
 
+def dig(target: str) -> dict:
+    cmd = ["dig", "google.com", f"@{target}"]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    parsed_output = parse_dig(result.stdout)
+
+    print(f"Ran dig using {target}")
+
+    return {"target": target, "stdout": result.stdout, "parsed_output": parsed_output}
+
+
 @app.celery.task
 def run_dig():
     """Run the dig command and update the global variable with the results."""
-    for target in app.targets:
-        cmd = ["dig", "google.com", f"@{target}"]
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(dig, app.targets))
 
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        app.redis.set(f"dig_{target}", result.stdout)
-
-        parsed_output = parse_dig(result.stdout)
+    for res in results:
+        target = res["target"]
+        app.redis.set(f"dig_{target}", res["stdout"])
 
         p = (
             Point("dig")
             .tag("target", target)
-            .field("query_time", parsed_output)
-            .field("raw", result.stdout)
+            .field("query_time", res["parsed_output"])
+            .field("raw", res["stdout"])
         )
 
         app.influx_write_api.write(
             bucket=environ["INFLUXDB_BUCKET"], org=environ["INFLUXDB_V2_ORG"], record=p
         )
-
-        print(f"Ran dig using {target}")
--- latency_monitor/mtr.py
+++ latency_monitor/mtr.py
@@ -1,4 +1,5 @@
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 from os import environ
 
 from influxdb_client import Point
@@ -7,35 +8,42 @@ from .latency_monitor import app
 from .utils import parse_mtr
 
 
+def mtr(target: str) -> dict:
+    cmd = ["mtr", "-rwznc", "10", target]
+    if ":" in target:
+        cmd = ["mtr", "-rwznc", "10", "-6", target]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    parsed_output = parse_mtr(result.stdout, target)
+
+    print(f"Ran MTR for {target}")
+
+    return {"target": target, "stdout": result.stdout, "parsed_output": parsed_output}
+
+
 @app.celery.task
 def run_mtr():
     """Run the MTR command and update the global variable with the results."""
-    for target in app.targets:
-        cmd = ["mtr", "-rwznc", "10", target]
-        # check if the target is an IPv4 or IPv6 address
-        if ":" in target:
-            cmd = ["mtr", "-rwznc", "10", "-6", target]
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(mtr, app.targets))
 
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        app.redis.set(f"mtr_{target}", result.stdout)
-
-        parsed_output = parse_mtr(result.stdout, target)
+    for res in results:
+        target = res["target"]
+        app.redis.set(f"mtr_{target}", res["stdout"])
 
         p = (
             Point("mtr")
             .tag("target", target)
-            .field("loss", parsed_output["loss"])
-            .field("snt", parsed_output["snt"])
-            .field("last", parsed_output["last"])
-            .field("avg", parsed_output["avg"])
-            .field("best", parsed_output["best"])
-            .field("worst", parsed_output["worst"])
-            .field("stdev", parsed_output["stdev"])
-            .field("raw", result.stdout)
+            .field("loss", res["parsed_output"]["loss"])
+            .field("snt", res["parsed_output"]["snt"])
+            .field("last", res["parsed_output"]["last"])
+            .field("avg", res["parsed_output"]["avg"])
+            .field("best", res["parsed_output"]["best"])
+            .field("worst", res["parsed_output"]["worst"])
+            .field("stdev", res["parsed_output"]["stdev"])
+            .field("raw", res["stdout"])
         )
 
         app.influx_write_api.write(
             bucket=environ["INFLUXDB_BUCKET"], org=environ["INFLUXDB_V2_ORG"], record=p
         )
-
-        print(f"Ran MTR for {target}")
